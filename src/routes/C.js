de5df43de10b829d25c05141bd74413b9c041c7f
fryad <fryadyazeez@gmail.com>
2023-06-17 22:16:47 +0300

    added message creation

--- src/routes/C.js
+++ src/routes/C.js
@@ -5,9 +5,15 @@ const path = "./src/data/data.json";
 
 router.get("/", (req, res) => {
     let data = Data.Read(path);
-    data.chat.M[0].m += 109746
-    data = Data.WriteR(path, data)
-    res.send(data.chat.M[0]);
+    res.send(data.chat);
+})
+
+router.post("/send", (req, res) => {
+    let Mes = req.body;
+    let data = Data.Read(path);
+    data.chat[Mes.chat_id].Mes.push(Mes.M)
+    Data.Write(path, data);
+    res.sendStatus(201);
 })
 
 module.exports = router;
