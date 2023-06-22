572c321ccbf48a8057917e9fb9aa1bbccc2b5468
fryad <fryadyazeez@gmail.com>
2023-06-22 13:29:45 +0300

    added cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const Croute = require("./routes/C.js")
+const cors = require("cors")
 const port = 3000
 
-
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
